8515be933341d236428d276de27d04d1d45244ed
Hasan Emre Doğan <h.emre.dgn@gmail.com>
2023-01-18 01:41:50 +0300

    first commit

---
+++ DataAccessLayer/Migrations/20230117223312_mig4.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Catagorys_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Catagorys",
+                table: "Catagorys");
+
+            migrationBuilder.RenameTable(
+                name: "Catagorys",
+                newName: "Categories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Catagorys");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Catagorys",
+                table: "Catagorys",
+                column: "CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Catagorys_CategoryID",
+                table: "Blogs",
+                column: "CategoryID",
+                principalTable: "Catagorys",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
